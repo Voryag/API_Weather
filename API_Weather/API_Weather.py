2d8032fe1c03fd198da8ee5e04a0d07d908a0d75
Voryag <sha4iklein@mail.ru>
2026-01-11 21:54:59 +0400

    MORE NEW

--- API_Weather/API_Weather.py
+++ API_Weather/API_Weather.py
@@ -13,7 +13,38 @@ class WeatherService:
     def __init__(self, api_key: str):
         self.api_key = api_key
         self._setup_logging()
-        self.logger.info("WearherService is initialiez")
+        self.logger.info("WearherService is initialized")
+
+    def _setup_logging(self):
+        self.logger = logging.getLogger(WeatherService)
+
+        if not self.logger.handlers:
+            self.logger.setLevel(logging.INFO)
+
+            formatter = logging.Formatter(
+                "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+                )
+
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(formatter)
+            self.logger.addHandler(console_handler)
+
+    def get_weather(self, city: str) -> dict | None:
+        self.logger.info(f"Запрос погоды для города: {city}")
+
+        params = {
+            "q": city_name,
+            "appid": api_key,
+            "units": "metric",
+            "lang": "ru",
+            }
+
+        try:
+            response = requests.get(self.BASE_URL, params=params, timeout=10)
+            response.raise_for_status()
+            
+            data = response.json()
+            self.logger.info(f"Получена информация для города: {city}")
 
     def get_weather_from_openweather(api_key, city_name="London"): 
         params = {
