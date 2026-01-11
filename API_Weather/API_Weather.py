50b2bfe84010b2831e34e923e59f4301df5958aa
Voryag <sha4iklein@mail.ru>
2026-01-11 22:11:46 +0400

    Create logger func

--- API_Weather/API_Weather.py
+++ API_Weather/API_Weather.py
@@ -1,3 +1,4 @@
+from matplotlib.pylab import f
 import requests
 import dotenv
 import logging
@@ -29,7 +30,7 @@ class WeatherService:
             console_handler.setFormatter(formatter)
             self.logger.addHandler(console_handler)
 
-    def get_weather(self, city: str) -> dict | None:
+    def get_weather(self, city: str) -> dict :
         self.logger.info(f"Запрос погоды для города: {city}")
 
         params = {
@@ -45,10 +46,46 @@ class WeatherService:
             
             data = response.json()
             self.logger.info(f"Получена информация для города: {city}")
+            return data
+
+        except requests.exceptions.Timeout:
+            self.logger.error(f"Таймайут при запросе для {city}")
+            print("Превышено время ожидания ответа от сервера")
+        except request.exceptions.ConnectionError:
+            self.logger.error(f"Ошибка подключения для {city}")
+            print("Проблема с подключением к интернету")
+        except request.exceptions.HTTPError as http_err:
+            error_data = response.json() if response else {}
+            error_msg = error_data.get("message", "Неизвестная ошибка API")
+
+            if response.status_code == 404:
+                self.logger.warning(f"Город не найден")
+                print(f"Город {city} не найден")
+            else:
+                self.logger.error(f"Ошибка API (код {response.status_code}) : {error_msg}")   
+                print(f"Ошибка API: {error_msg}")
+        except Exception as e:
+            self.logger.critical(f"Непредвиденная ошибка: {e}", exc_info=True)
+            print(f"Ошибка API: {error_msg}")
+
+        return None
+
+    def display_weather(self, weather_data: dict) -> None:
+        if not weather_data:
+            print("Нет данных для отображения")
+            return
+
+        city = weather_data["name"]
+        temp = weather_data["main"]["temp"]
+        humidity = weather_data["main"]["humidity"]
+        description = weather_data["weather"][0]["description"]
+        feels_like = weather_data["main"].get("feels_like", temp)
+
+        print(f"\n{'='*40}")
 
     def get_weather_from_openweather(api_key, city_name="London"): 
         params = {
-            "q": city_name,
+            'q': city_name,
             "appid": api_key,
             "units": "metric",
             "lang": "ru",
